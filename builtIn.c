f9f3a89ef75024e7dba4ff6e0ecf1807b92a478a
ngwenyatm <thabsnspaces@gmail.com>
2023-11-15 13:42:36 -0800

    built in func

---
+++ builtIn.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+*builtIn - checks if input is env or exit and executes accordingly
+*@args: tokenized string array from user
+*@envp: environment info
+*Return: 0
+*/
+int builtIn(char **args, char **envp)
+{
+	if (args[0] == "env")
+	{
+		int i = 0;
+
+		for (i = 0 ; envp[i] != NULL; i++)
+		{
+			write(STDOUT_FILENO, (const void *)envp[i], _strlen(envp[i]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		return (1);
+	}
+	else if (args[0] == "exit")
+	{
+		exit(0);
+		return (1);
+	}
+
+	return (0);
+}
